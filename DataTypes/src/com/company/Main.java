fd0db2d2eb5d17cbe4e0b5a731767db2982de53f
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-08 13:39:16 +0530

    Create new project to solve DataTypes problem on HackerRank

--- DataTypes/src/com/company/Main.java
+++ DataTypes/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,5 +15,56 @@ public class Main {
         //Given an input integer, you must determine which primitive data types are capable of properly storing that
         // input.
         //To get you started, a portion of the solution is provided for you in the editor.
+        // Input Format
+        //The first line contains an integer,T , denoting the number of test cases.
+        //Each test case, T, is comprised of a single line with an integer, n, which can be arbitrarily large or small.
+        //Output Format
+        //For each input variable n and appropriate primitive datatype you must determine if the given primitives are
+        // capable of storing it. If yes, then print:
+        //n can be fitted in:
+        //* dataType
+
+        // sample output
+        // -150 can be fitted in:
+        //* short
+        //* int
+        //* long
+        //150000 can be fitted in:
+        //* int
+        //* long
+        //1500000000 can be fitted in:
+        //* int
+        //* long
+        //213333333333333333333333333333333333 can't be fitted anywhere.
+        //-100000000000000 can be fitted in:
+        //* long
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE)System.out.println("* byte");
+                if (x>=Short.MIN_VALUE && x<=Short.MAX_VALUE)System.out.println("* short");
+                if (x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE)System.out.println("* int");
+                if (x>=Long.MIN_VALUE && x<=Long.MAX_VALUE)System.out.println("* long");
+
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+
+        Integer n = 100;
+        String s = String.valueOf(n);
+        System.out.println(s);
+
     }
 }
