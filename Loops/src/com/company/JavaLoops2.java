44d1889c4ee23e9d9ab5a9a40042f8553bd41276
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-08 12:46:04 +0530

    Solve Loops challenges on hacker-Rank

--- Loops/src/com/company/JavaLoops2.java
+++ Loops/src/com/company/JavaLoops2.java
@@ -6,7 +6,8 @@ public class JavaLoops2 {
     public static void main(String[] args) {
         /*
         We use the integers a, b, and n to create the following series:
-        (a + (2^0).b), (a + (2^0).b + (2^1).b), (a + (2^0).b + (2^1).b)
+        (a + (2^0)*b), (a + (2^0)*b + (2^1)*b), (a + (2^0)*b + (2^1)*b + (2^2)*b), .......(a + (2^0)*b +
+        (2^1)*b + (2^2)*b......(2^n-1)*b)
 
         You are given  queries in the form of a, b, and n. For each query, print the series corresponding to the given , ,
         and values as a single line of  space-separated integers.
