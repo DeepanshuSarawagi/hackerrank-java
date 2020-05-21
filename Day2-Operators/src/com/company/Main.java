fea195a86d64a24c4ed159e64f7457968193bbd9
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-21 21:34:28 +0530

    Fix solution - Day2 Challenge on Hackerrank.

--- Day2-Operators/src/com/company/Main.java
+++ Day2-Operators/src/com/company/Main.java
@@ -23,7 +23,7 @@ public class Main {
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
         double tip = (meal_cost * tip_percent) / 100;
         double tax = (meal_cost * tax_percent) / 100;
-        int totalCost = (int)(meal_cost + tip + tax);
+        int totalCost = (int) Math.round(meal_cost + tip + tax);
         System.out.println(totalCost);
 
     }
