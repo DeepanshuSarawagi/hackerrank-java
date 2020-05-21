9de1c05b54b42c69d716538e82bfcaf1cc90baff
Deepanshu Sarawagi <deepanshusarawagi@gmail.com>
2020-05-21 22:57:57 +0530

    Create new project to solve Day6 challenge

--- Day6-LetsReview/src/com/company/Main.java
+++ Day6-LetsReview/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a string of your choice: ");
+        String s = scanner.nextLine();
+        for (int i=0; i<s.length(); i+=2) {
+            System.out.print(s.charAt(i));
+        }
+        System.out.print(" ");
+        for (int i=1; i<s.length(); i+=2) {
+            System.out.print(s.charAt(i));
+        }
     }
 }
